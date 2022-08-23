46575561b10905bc23624793dbd5da43ea6ba4a3
Alexpuen <alexanderpuentes2004@gmail.com>
2022-08-23 17:17:09 -0500

    Ejemplo de funciones decoradoras

---
+++ ejemplo_decoradores.py
@@ -0,0 +1,31 @@
+def funcion_decoradora(funcion_parametro):
+    def funcion_interior():
+
+        print("vamos a realizar un cálculo: ")
+
+        funcion_parametro()
+
+
+        print("hemos terminado el cálculo")
+
+    return funcion_interior
+
+
+
+
+
+
+
+@funcion_decoradora
+def suma():
+
+    print(15+20)
+
+@funcion_decoradora
+def resta():
+    print (30-10)
+
+
+suma()
+
+resta()
